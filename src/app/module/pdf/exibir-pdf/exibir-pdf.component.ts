97c406d008e29033b86125db5ba0785227db3049
Carlos <carlosneyrbjunior@gmail.com>
2022-11-18 11:13:49 -0300

    Atualização do back-end e interação entre componentes e módulos

---
+++ src/app/module/pdf/exibir-pdf/exibir-pdf.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+
+import { ServicoPDFService } from './../../../servico-pdf.service';
+import { PerguntasRespostas } from '../../perguntasrespostas';
+
+@Component({
+  selector: 'app-exibir-pdf',
+  templateUrl: './exibir-pdf.component.html',
+  styleUrls: ['./exibir-pdf.component.scss']
+})
+export class ExibirPDFComponent implements OnInit {
+  listaAlunos: string[] = [];
+  listaPerguntasRespostas: PerguntasRespostas[] = [];
+
+  @Input('perguntas') listaPerguntas : string[] = [];
+
+  @Output() retornarBool = new EventEmitter()
+  constructor(private servicoPDF: ServicoPDFService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  retornar() {
+    this.retornarBool.emit({aba: 'PDF'})
+  }
+
+  exportar() {
+    this.servicoPDF.exportaPDF(this.listaAlunos, this.listaPerguntasRespostas);
+  }
+}
