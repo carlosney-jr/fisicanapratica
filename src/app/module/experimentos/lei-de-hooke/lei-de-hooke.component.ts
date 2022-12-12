efcb70167015fcd7ef3607e19bd301b0d78857ec
Carlos Ney do Rego Bezerra Júnior <c.ney@academico.ifrn.edu.br>
2022-12-12 11:14:18 -0300

    Inserção dos experimentos de Transformadores e Lei de Hooke

--- src/app/module/experimentos/lei-de-hooke/lei-de-hooke.component.ts
+++ src/app/module/experimentos/lei-de-hooke/lei-de-hooke.component.ts
@@ -1,4 +1,8 @@
+import { PerguntaFilha } from './../../PerguntaFilha';
+import { TabelaPerguntas } from './../../Tabela';
+import { PerguntasRespostas } from './../../PerguntaRespostas';
 import { Component } from '@angular/core';
+import { Pergunta } from '../../Pergunta';
 
 @Component({
   selector: 'app-lei-de-hooke',
@@ -6,5 +10,93 @@ import { Component } from '@angular/core';
   styleUrls: ['./lei-de-hooke.component.scss']
 })
 export class LeiDeHookeComponent {
+  private _aba: string = "experimento"
 
+  get aba() {
+    return this._aba;
+  }
+
+  set aba(aba: string) {
+    if (aba=="experimento" || aba=="PDF") {
+      this._aba = aba;
+    } else {
+      return
+    }
+  }
+
+  configurarAba(evento: string){
+    this._aba = evento;
+  }
+
+  public questoes: PerguntasRespostas<string>[] = [
+    new TabelaPerguntas({
+      key: '1',
+      order: 1,
+      label: "Para cada massor usado, preencha abaixo com as informações de cada tentativa, incluindo peso do massor usado (N), força elástica, elongação (cm) e constante elástica (N/cm).",
+      listaPerguntas: [
+        new PerguntaFilha({
+          key: "A",
+          order: 1,
+          label: "Na 1ª tentativa, foi encontrado:"
+        }),
+        new PerguntaFilha({
+          key: "B",
+          order: 2,
+          label: "Na 2ª tentativa, foi encontrado:"
+        }),
+        new PerguntaFilha({
+          key: "C",
+          order: 3,
+          label: "Na 3ª tentativa, foi encontrado:"
+        }),
+        new PerguntaFilha({
+          key: "D",
+          order: 4,
+          label: "Na 4ª tentativa, foi encontrado:"
+        })
+      ]
+    }),
+    new TabelaPerguntas({
+      key: '2',
+      order: 2,
+      label: "Agora transforme as contantes elásticas de N/cm para N/m (unidade padrão)",
+      listaPerguntas: [
+        new PerguntaFilha({
+          key: 'A',
+          order: 1,
+          label: "Na 1ª tentativa, a constante elástica, agora convertida em N/m, é:"
+        }),
+        new PerguntaFilha({
+          key: 'B',
+          order: 2,
+          label: "Na 2ª tentativa, a constante elástica, agora convertida em N/m, é:"
+        }),
+        new PerguntaFilha({
+          key: 'C',
+          order: 3,
+          label: "Na 3ª tentativa, a constante elástica, agora convertida em N/m, é:"
+        }),
+        new PerguntaFilha({
+          key: 'D',
+          order: 4,
+          label: "Na 4ª tentativa, a constante elástica, agora convertida em N/m, é:"
+        })
+      ]
+    }),
+    new Pergunta({
+      key: '3',
+      order: 3,
+      label: "Calcule a média dos valores encontrados para a constante elástica em N/m. Calculando a média elimina-se uma parcela dos erros de leitura por parte do pesquisador ou por limitações dos instrumentos de medida. Dessa forma, a média representa o valor mais próximo do real." 
+    }),
+    new Pergunta({
+      key: '4',
+      order: 4,
+      label: "Construa um gráfico da Força Elástica N/cm em função da Elongação (cm) e envie em anexo à professora responsável. Através deste gráfico, estime o valor da constante elástica:"
+    }),
+    new Pergunta({
+      key: '5',
+      order: 5,
+      label: "Nesse experimento, pode-se dizer que a Lei de Hooke foi verificada? Por quê?"
+    })
+  ]
 }
--- src/app/module/experimentos/transformadores/transformadores.component.ts
+++ src/app/module/experimentos/transformadores/transformadores.component.ts
@@ -12,41 +12,6 @@ import { TabelaPerguntas } from '../../Tabela';
 })
 export class TransformadoresComponent {
   private _aba: string = "experimento"
-  public questoes: PerguntasRespostas<string>[] = [
-    new Pergunta({
-      key: '1',
-      label: "Como foi possível perceber o efeito 'Monga'?",
-      order: 1
-    }),
-    new Pergunta({
-      key: '2',
-      label: "Insira as anotações daquilo lá",
-      order: 2
-    }),
-    new Imagem({
-      key: 'Img1',
-      label: "Figura 1 - Logo do IF",
-      resposta: "assets/imgs/Navbar/LogoOficial.png",
-      order: 3
-    }),
-    new TabelaPerguntas({
-      key: '3',
-      label: "Insira as seguintes informações:",
-      order: 4,
-      listaPerguntas: [
-        new PerguntaFilha({
-          key: "A",
-          label: "Se o caminho do feixe de luz incidente for paralelo ao eixe óptico, qual o caminho do feixe de luz emergente?",
-          order: 1
-        }),
-        new PerguntaFilha({
-          key: "B",
-          label: "Se o caminho do feixe de luz incidente for através do eixo F, qual o caminho do feixe de luz emergente?",
-          order: 2
-        })
-      ]
-    })
-  ]
 
   get aba() {
     return this._aba;
@@ -63,5 +28,43 @@ export class TransformadoresComponent {
   configurarAba(evento: string){
     this._aba = evento;
   }
+  public questoes: PerguntasRespostas<string>[] = [
+    new Pergunta({
+      key: '1',
+      label: "O que foi observado na deflexão do galvanômetro, após a movimentação das bobinas?",
+      order: 1
+    }),
+    new Pergunta({
+      key: '2',
+      label: 'Após colocar o núcleo em "I" na bobina de campo, tal como na figura 7, o que pode ser relatado?',
+      order: 2
+    }),
+    new Imagem({
+      key: 'Img1',
+      label: "",
+      resposta: "assets/imgs/Experimentos/Transformador/TRA0708.png",
+      order: 3
+    }),
+    new Pergunta({
+      key: '3',
+      label: "Como na figura 7, quando as bobinas foram postas próximas umas as outras e o interruptor foi aberto e fechado, o que pode ser relatado?",
+      order: 4
+    }),
+    new Pergunta({
+      key: '4',
+      label: "Após colocar o núcleo em ambas as bobinas na mesma distância e ter fechado e aberto o interruptor novamente, o que ocorreu?",
+      order: 5
+    }),
+    new Pergunta({
+      key: '5',
+      order: 6,
+      label: "Depois de ter colocado a bobina de campo e a de indução no núcleo em 'U' (como na figura 8) e ter aberto e fechado o interruptor, o que aconteceu?"
+    }),
+    new Pergunta({
+      key: '6',
+      order: 7,
+      label: "O que ocorreu quando foi alterada a tensão da fonte de alimentação com o interruptor fechado?"
+    })
+  ]
 
 }
