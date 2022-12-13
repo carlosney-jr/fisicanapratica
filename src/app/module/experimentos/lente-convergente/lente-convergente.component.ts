494bff9b5a494c33838ec51b6d9d21e36992fb2b
Carlos Ney do Rego Bezerra Júnior <c.ney@academico.ifrn.edu.br>
2022-12-13 10:20:39 -0300

    Inicio da Lente Convergente

--- src/app/module/experimentos/lente-convergente/lente-convergente.component.ts
+++ src/app/module/experimentos/lente-convergente/lente-convergente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PerguntasRespostas } from '../../PerguntaRespostas';
 
 @Component({
   selector: 'app-lente-convergente',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./lente-convergente.component.scss']
 })
 export class LenteConvergenteComponent {
+  private _aba: string = "experimento"
+  
+  get aba() {
+    return this._aba;
+  }
 
+  set aba(aba: string) {
+    if (aba=="experimento" || aba=="PDF") {
+      this._aba = aba;
+    } else {
+      return
+    }
+  }
+
+  configurarAba(evento: string){
+    this._aba = evento;
+  }
+  public questoes: PerguntasRespostas<string>[] = [
+    
+  ]
 }
